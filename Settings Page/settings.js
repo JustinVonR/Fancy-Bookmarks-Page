6da9919bc652b6477b3d93882c143111eb087350
Justin von Rosenberg <jvonr43@gmail.com>
2024-01-09 23:05:52 -0700

    add first part of settings page logic

--- Settings Page/settings.js
+++ Settings Page/settings.js
@@ -1,16 +1,3 @@
-let settings=browser.storage.sync.get({
-    theme: "DARK",
-    accent: "#ce35ce",
-    bgType: "COLOR",
-    bgFile: "No File Selected",
-    bgColor: "#000000"
-});
-settings.then(onGotSettings, onError);
-
-function onGotSettings() {
-    
-}
-
 function onError(error) {
     console.log(error);
 }
@@ -19,3 +6,44 @@ function onError(error) {
 function handleThemeChange(themeRadio) {
     browser.storage.sync.set(settings);
 }
+
+async function init() {
+    try {
+        settings= await browser.storage.sync.get({
+            theme: "DARK",
+            accent: "#ce35ce",
+            bgType: "COLOR",
+            bgFile: "No File Selected",
+            bgColor: "#000000"
+        });
+        applyCurrentSettings(settings);
+    } catch (error) {
+        onError(error);
+    }
+    initEventListeners();
+}
+
+function applyCurrentSettings() {
+
+}
+
+function initEventListeners() {
+    let settingsForm = document.getElementsByClassName("settingsForm");
+    settingsForm.addEventListener('change', handleFormChange(target));
+}
+
+function getRadioVal(radioName) {
+    let radioElements = Array.from(document.querySelectorAll(`input[name=${radioName}]`));
+    let value;
+    for (element in radioElements) {
+        if (element.checked) {
+            value = element.value;
+        }
+    }
+    return value;
+}
+
+function handleFormChange(target) {
+    console.log(`The target ${target} was interacted with.`);
+}
+
