976fa21a58ad04007fb9e6a70fcb3e667420f051
Justin von Rosenberg <jvonr43@gmail.com>
2024-05-15 12:59:25 -0600

    Remove extraneous prints from settings js

--- Bookmarks Page/fullPage.js
+++ Bookmarks Page/fullPage.js
@@ -1,2 +1,30 @@
-console.log("Script Loaded");
-console.log("Other Thing")
+function loadTheming() {
+    function applySettings(result) {
+        //Apply the correct stylesheet for the selected theme:
+        let styleLink = document.createElement("link");
+        styleLink.rel = "stylesheet";
+        styleLink.type = "text/css";
+        if (result.theme == "DARK") {
+            styleLink.href = "darkTheme.css";
+        } else {
+            styleLink.href = "lightTheme.css";
+        }
+        document.head.appendChild( styleLink );
+
+        console.log(document.querySelector("div.links"));
+        document.querySelector("div.link-region").style = `background-color: ${result.bgColor}`;
+        linkTiles = document.querySelectorAll("div.link");
+        linkTiles.forEach(element => {
+            element.style = `color: ${result.accent}`
+        });
+    }
+
+    function onSettingsError(error) {
+        console.log(`Error while getting settings: \n ${error}`);
+    }
+
+    let getSettings = browser.storage.sync.get();
+    getSettings.then(applySettings, onSettingsError);
+}
+
+document.addEventListener("DOMContentLoaded", loadTheming);
